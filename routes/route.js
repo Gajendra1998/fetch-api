768f7b274fd8d552dfa5a27c34cfae95c697f4e8
Gajendra <gajendrasingh80300@gmail.com>
2021-07-10 12:04:12 +0530

    some changes

--- routes/route.js
+++ routes/route.js
@@ -5,6 +5,7 @@ const Hodlinginfo = require('../modals/schema')
 
 router.get('/',(req,res)=>{
     const result= Hodlinginfo.find();
+    JSON.stringify(result)
     res.json(result);    
 })
 module.exports=router;
